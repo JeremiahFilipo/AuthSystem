6892671d2ad874c0fd4d0194fde5a5e324ff30d7
Jeremiah Filipo <ac98586@avcol.school.nz>
2020-09-04 12:28:30 +1200

    Added Teachers Students and Departments page

---
+++ Views/Shared/Departments.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AuthSystem.Views.Shared
+{
+    public class DepaertmentsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
