4c7f837199b6fc5d024947874d8e29c726ecb949
Jeremiah Filipo <ac98586@avcol.school.nz>
2020-11-03 09:42:26 +1300

    Few Touch Ups on pages for students, teachers and departments

---
+++ Models/Departments.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Models
+{
+    public class Departments
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+
+    }
+}
---
+++ Models/Students.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+
+    }
+}
+
